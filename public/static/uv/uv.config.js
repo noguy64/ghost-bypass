f8ba861bdf33f8e024b8cd41d4286a752351f8d1
noguy64 <samtammi32@gmail.com>
2026-03-05 00:09:28 -0500

    Update bare URL in uv.config.js

--- public/static/uv/uv.config.js
+++ public/static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://bare.benrogo.net',
+    bare:'https://sigma-bare.yourmom.eu.org',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
